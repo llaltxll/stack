c23d71bfb0b1c60bbaf698541a5304734859227c
llaltxll <llaltxll@gmail.com>
2022-12-31 09:17:50 +0200

    make it better

--- stack/main.c
+++ stack/main.c
@@ -2,15 +2,41 @@
 #include <stdlib.h>
 
 #define INIT_SIZE 2
-#define INIT_ELEMS 1024
+/* tested up to 132000000 on ryzen 9 with 32GB  direct input required */
+#define INIT_ELEMS 1024 
 
 typedef struct  {
-	int size;
-	int* arr;
-	int used;
+	size_t size;
+	size_t* arr;
+	size_t used;
 }stack ;
 
-stack *alloc_stack(stack *s, int size)
+stack* alloc_stack(stack* s, size_t size);
+void resize_stack(stack* s, size_t new_size);
+void print_stack(stack* s);
+void push_stack(stack* s, size_t element);
+size_t pop_stack(stack* s);
+
+int main()
+{
+	size_t i;
+	stack *my_stack = NULL;
+	my_stack = (stack*)alloc_stack(my_stack, INIT_SIZE);
+	print_stack(my_stack);
+	for (i = 0; i < INIT_ELEMS; i++)
+		push_stack(my_stack, i + 1);
+	print_stack(my_stack);
+	while (my_stack->used)
+		printf("%d\n", pop_stack(my_stack));
+	print_stack(my_stack);
+
+	free(my_stack->arr);
+	free(my_stack);
+	/* according to the ISO manual nothing should happen if ptr is NULL */
+	return 0;
+}
+
+stack* alloc_stack(stack* s, size_t size)
 {
 	s = (stack*)malloc(sizeof(stack));
 	if (s == NULL)
@@ -18,7 +44,7 @@ stack *alloc_stack(stack *s, int size)
 		printf("malloc1");
 		exit(0);
 	}
-	s->arr = (int*)malloc(sizeof(int) * size);
+	s->arr = (size_t*)malloc(sizeof(size_t) * size);
 	if (s->arr == NULL)
 	{
 		printf("malloc2");
@@ -28,10 +54,10 @@ stack *alloc_stack(stack *s, int size)
 	s->used = 0;
 	return s;
 }
-void resize_stack(stack* s, int new_size)
+void resize_stack(stack* s, size_t new_size)
 {
-	int* tmp;
-	tmp = (int*)realloc(s->arr,sizeof(int) * new_size);
+	size_t* tmp;
+	tmp = (size_t*)realloc(s->arr, sizeof(size_t) * new_size);
 	if (s->arr == NULL)
 	{
 		printf("realloc");
@@ -42,45 +68,28 @@ void resize_stack(stack* s, int new_size)
 }
 void print_stack(stack* s)
 {
-	int i;
+	size_t i;
 	char separator = ' ';
 	printf("{");
-	for (i = 0;i < s->used; i++)
+	for (i = 0; i < s->used; i++)
 	{
-		printf("%c%d ", separator, s->arr[i]);
+		printf("%c%ld ", separator, s->arr[i]);
 		separator = ',';
 	}
 	printf("}\n");
 }
-void push_stack(stack* s, int element)
+void push_stack(stack* s, size_t element)
 {
 	if (s->used == s->size)
 		resize_stack(s, s->size * 2);
 	s->arr[s->used] = element;
 	s->used++;
 }
-int pop_stack(stack* s)
+size_t pop_stack(stack* s)
 {
 	int ret = s->arr[s->used - 1];
 	s->used--;
 	if (s->used && s->size / s->used > 1)
 		resize_stack(s, s->size / 2);
 	return ret;
-}
-int main()
-{
-	int i,j;
-	stack *my_stack = NULL;
-	my_stack = (stack*)alloc_stack(my_stack, INIT_SIZE);
-	//print_stack(my_stack);
-	for (i = 0; i < INIT_ELEMS; i++)
-		push_stack(my_stack, i + 1);
-	print_stack(my_stack);
-	while (my_stack->used)
-		printf("%d\n", pop_stack(my_stack));
-	print_stack(my_stack);
-
-	free(my_stack->arr);
-	free(my_stack);
-	return 0;
 }
